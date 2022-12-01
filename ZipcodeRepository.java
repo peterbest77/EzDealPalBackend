748360c92d477b77b7c744a4c39a99600aea81b8
CorneliaChu <90665491+CorneliaChu@users.noreply.github.com>
2022-11-30 22:37:39 -0600

    Delete ZipcodeRepository.java

--- ZipcodeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.laioffer.ezdealpal.repository;
-
-import com.laioffer.ezdealpal.entity.ZipcodeMap;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ZipcodeRepository extends JpaRepository<ZipcodeMap, String> {
-    ZipcodeMap getZipcodeMapByZipcode(String zipcode);
-}

e81b0ecb494a134445862ed5e1f212fb5959bdf3
CoconutNami <107953499+CoconutNami@users.noreply.github.com>
2022-12-07 01:09:01 -0600

    new code

--- EzDealPal/src/main/java/com/laioffer/ezdealpal/HelloServlet.java
+++ EzDealPal/src/main/java/com/laioffer/ezdealpal/HelloServlet.java
@@ -1,5 +1,10 @@
 package com.laioffer.ezdealpal;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.ezdealpal.entity.User;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
 import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -13,13 +18,47 @@ public class HelloServlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+//        response.setContentType("text/html");
+//
+//        // Hello
+//        PrintWriter out = response.getWriter();
+//        out.println("<html><body>");
+//        out.println("<h1>" + message + "</h1>");
+//        out.println("</body></html>");
+        response.setContentType("application/json");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        User c = new User();
+        c.setUserId("sun@laioffer.com");
+        c.setPassword("123456");
+        c.setName("rick");
+        c.setEnabled(true);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        response.getWriter().print(mapper.writeValueAsString(c));
+
+
+        JSONObject customer = new JSONObject();
+        customer.put("userId", "sun@laioffer.com");
+        customer.put("name", "rick");
+        customer.put("age", 50);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // Read customer information from request body
+        JSONObject jsonRequest = new JSONObject(IOUtils.toString(request.getReader()));
+        String email = jsonRequest.getString("userId");
+        String name = jsonRequest.getString("name");
+
+        // Print customer information to IDE console
+        System.out.println("Email is: " + email);
+        System.out.println("name is: " + name);
+
+        // Return status = ok as response body to the client
+        response.setContentType("application/json");
+        JSONObject jsonResponse = new JSONObject();
+        jsonResponse.put("status", "ok");
+        response.getWriter().print(jsonResponse);
     }
 
     public void destroy() {
--- EzDealPal/src/main/java/com/laioffer/ezdealpal/controller/SignInController.java
+++ EzDealPal/src/main/java/com/laioffer/ezdealpal/controller/SignInController.java
@@ -15,7 +15,7 @@ public class SignInController {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    // we only process the failed login request here, if login successfully, it will automatically redirect to home page
+     // xwe only process the failed login request here, if login successfully, it will automatically redirect to home page
     @RequestMapping("/login")
     public void login(@RequestParam(value = "error") String error, HttpServletResponse response) throws IOException {
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
